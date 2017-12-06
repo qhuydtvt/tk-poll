3bd08f1b1b81ab8b17b796f994d0e67495d1ca6f
huynq <qhuydtvt@gmail.com>
2017-12-06 07:31:17 +0700

    .

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/create_poll')
+def create_poll():
+    return render_template("poll_create.html")
+
 if __name__ == '__main__':
   app.run(debug=True)
